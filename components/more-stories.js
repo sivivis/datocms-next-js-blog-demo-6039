e147146fb90c0e3542292364dbdd8e298d50bf30
sivivis <sivivis@users.noreply.github.com>
2020-11-18 10:05:22 +1000

    Update more-stories.js

--- components/more-stories.js
+++ components/more-stories.js
@@ -11,7 +11,6 @@ export default function MoreStories({ posts }) {
           <PostPreview
             key={post.slug}
             title={post.title}
-            coverImage={post.coverImage}
             date={post.date}
             author={post.author}
             slug={post.slug}
