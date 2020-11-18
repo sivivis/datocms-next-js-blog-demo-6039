68153001c196c3ccc5831f250e5c4395b28e7a5d
sivivis <sivivis@users.noreply.github.com>
2020-11-18 10:27:58 +1000

    fgh
    dfg

--- components/more-stories.js
+++ components/more-stories.js
@@ -11,6 +11,7 @@ export default function MoreStories({ posts }) {
           <PostPreview
             key={post.slug}
             title={post.title}
+            coverImage={post.coverImage}
             date={post.date}
             author={post.author}
             slug={post.slug}
